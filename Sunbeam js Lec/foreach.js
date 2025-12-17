297e4ea5b395ea3d1aaf82aedbb1f9d18684c0a3
Sairaj <ekbotesairaj640@gmail.com>
2025-12-17 08:48:47 +0530

    files organised

---
+++ Sunbeam js Lec/foreach.js
@@ -0,0 +1,18 @@
+const arr1 = []
+
+arr1.push(34)
+arr1.push(23)
+arr1.push(1)
+arr1.push(2)
+arr1.push(2)
+
+// normal iteration using for of loop
+console.log("Iteration using for of : ")
+for(const el of arr1)
+    console.log(el)
+
+// iteration using foreach  method 
+// Note: foreach only used for validation 
+// foreach not used for value processing
+console.log("Iteration using foreach method: ")
+arr1.forEach((val)=>console.log(val))
